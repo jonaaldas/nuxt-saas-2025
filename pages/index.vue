119d061b31a27beccbc5369d12abd4f49bbff9a3
Jonathan Aldas <jonaaldas@gmail.com>
2024-12-20 18:05:21 +0100

    landing page done

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,33 @@
-<template>
-  <div>
-    <h1>Hello World</h1>
-    <Button>Click me</Button>
-  </div>
-</template>
-
 <script setup lang="ts">
-import { Button } from "@/components/ui/button";
+import Navbar from "../components/landing/Navbar.vue";
+import Hero from "../components/landing/Hero.vue";
+import Benefits from "../components/landing/Benefits.vue";
+import Features from "../components/landing/Features.vue";
+import Services from "../components/landing/Services.vue";
+import HowItWorks from "../components/landing/HowItWorks.vue";
+import Sponsors from "../components/landing/Sponsors.vue";
+import Testimonials from "../components/landing/Testimonials.vue";
+import Team from "../components/landing/Team.vue";
+import Pricing from "../components/landing/Pricing.vue";
+import Community from "../components/landing/Community.vue";
+import Contact from "../components/landing/Contact.vue";
+import FAQ from "../components/landing/FAQ.vue";
+import Footer from "../components/landing/Footer.vue";
 </script>
 
-<style scoped></style>
+<template>
+  <Navbar />
+  <Hero />
+  <Sponsors />
+  <Benefits />
+  <Features />
+  <Services />
+  <HowItWorks />
+  <Testimonials />
+  <Team />
+  <Community />
+  <Pricing />
+  <Contact />
+  <FAQ />
+  <Footer />
+</template>
