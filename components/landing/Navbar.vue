7498bcc6ad3a47aa27587b8d717e23f906632190
Jonathan Aldas <jonaaldas@gmail.com>
2024-12-20 18:15:57 +0100

    hamburger issue

--- components/landing/Navbar.vue
+++ components/landing/Navbar.vue
@@ -13,14 +13,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu";
-import {
-  Sheet,
-  SheetContent,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -65,13 +58,11 @@ const featureList: FeatureProps[] = [
   },
   {
     title: "Build Trust",
-    description:
-      "Leverages social proof elements to establish trust and credibility.",
+    description: "Leverages social proof elements to establish trust and credibility.",
   },
   {
     title: "Capture Leads",
-    description:
-      "Make your lead capture form visually appealing and strategically.",
+    description: "Make your lead capture form visually appealing and strategically.",
   },
 ];
 
@@ -84,41 +75,26 @@ const isOpen = ref<boolean>(false);
       'shadow-light': mode === 'light',
       'shadow-dark': mode === 'dark',
       'w-[90%] md:w-[70%] lg:w-[75%] lg:max-w-screen-xl top-5 mx-auto sticky border z-40 rounded-2xl flex justify-between items-center p-2 bg-card shadow-md': true,
-    }"
-  >
-    <a
-      href="/"
-      class="font-bold text-lg flex items-center"
-    >
+    }">
+    <a href="/" class="font-bold text-lg flex items-center">
       <ChevronsDown
-        class="bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white"
-      />
+        class="bg-gradient-to-tr from-primary via-primary/70 to-primary rounded-lg w-9 h-9 mr-2 border text-white" />
       ShadcnVue</a
     >
     <!-- Mobile -->
-    <div class="flex items-center lg:hidden">
+    <div class="flex items-center md:!hidden">
       <Sheet v-model:open="isOpen">
         <SheetTrigger as-child>
-          <Menu
-            @click="isOpen = true"
-            class="cursor-pointer"
-          />
+          <Menu @click="isOpen = true" class="cursor-pointer" />
         </SheetTrigger>
 
-        <SheetContent
-          side="left"
-          class="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card"
-        >
+        <SheetContent side="left" class="flex flex-col justify-between rounded-tr-2xl rounded-br-2xl bg-card">
           <div>
             <SheetHeader class="mb-4 ml-4">
               <SheetTitle class="flex items-center">
-                <a
-                  href="/"
-                  class="flex items-center"
-                >
+                <a href="/" class="flex items-center">
                   <ChevronsDown
-                    class="bg-gradient-to-tr from-primary/70 via-primary to-primary/70 rounded-lg size-9 mr-2 border text-white"
-                  />
+                    class="bg-gradient-to-tr from-primary/70 via-primary to-primary/70 rounded-lg size-9 mr-2 border text-white" />
                   ShadcnVue
                 </a>
               </SheetTitle>
@@ -130,12 +106,8 @@ const isOpen = ref<boolean>(false);
                 :key="label"
                 as-child
                 variant="ghost"
-                class="justify-start text-base"
-              >
-                <a
-                  @click="isOpen = false"
-                  :href="href"
-                >
+                class="justify-start text-base">
+                <a @click="isOpen = false" :href="href">
                   {{ label }}
                 </a>
               </Button>
@@ -144,7 +116,6 @@ const isOpen = ref<boolean>(false);
 
           <SheetFooter class="flex-col sm:flex-col justify-start items-start">
             <Separator class="mb-2" />
-
             <ToggleTheme />
           </SheetFooter>
         </SheetContent>
@@ -152,25 +123,18 @@ const isOpen = ref<boolean>(false);
     </div>
 
     <!-- Desktop -->
-    <NavigationMenu class="hidden lg:block">
+    <NavigationMenu class="hidden md:block">
       <NavigationMenuList>
         <NavigationMenuItem>
-          <NavigationMenuTrigger class="bg-card text-base">
-            Features
-          </NavigationMenuTrigger>
+          <NavigationMenuTrigger class="bg-card text-base"> Features </NavigationMenuTrigger>
           <NavigationMenuContent>
             <div class="grid w-[600px] grid-cols-2 gap-5 p-4">
-              <img
-                src="https://www.radix-vue.com/logo.svg"
-                alt="Beach"
-                class="h-full w-full rounded-md object-cover"
-              />
+              <img src="https://www.radix-vue.com/logo.svg" alt="Beach" class="h-full w-full rounded-md object-cover" />
               <ul class="flex flex-col gap-2">
                 <li
                   v-for="{ title, description } in featureList"
                   :key="title"
-                  class="rounded-md p-3 text-sm hover:bg-muted"
-                >
+                  class="rounded-md p-3 text-sm hover:bg-muted">
                   <p class="mb-1 font-semibold leading-none text-foreground">
                     {{ title }}
                   </p>
@@ -190,8 +154,7 @@ const isOpen = ref<boolean>(false);
               :key="label"
               as-child
               variant="ghost"
-              class="justify-start text-base"
-            >
+              class="justify-start text-base">
               <a :href="href">
                 {{ label }}
               </a>
@@ -204,17 +167,11 @@ const isOpen = ref<boolean>(false);
     <div class="hidden lg:flex">
       <ToggleTheme />
 
-      <Button
-        as-child
-        size="sm"
-        variant="ghost"
-        aria-label="View on GitHub"
-      >
+      <Button as-child size="sm" variant="ghost" aria-label="View on GitHub">
         <a
           aria-label="View on GitHub"
           href="https://github.com/leoMirandaa/shadcn-vue-landing-page.git"
-          target="_blank"
-        >
+          target="_blank">
           <GithubIcon class="size-5" />
         </a>
       </Button>
