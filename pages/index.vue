7535c4bb2abca847544c0df131a3e5a054b03541
Jonathan Aldas <jonaaldas@gmail.com>
2024-12-20 17:52:32 +0100

    new shadcn components

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <Button>Click me</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<style scoped></style>
